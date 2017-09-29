de8e1a54b563b811c7e1a503c2086fb6ed325762
Zorca <vs@zorca.org>
2017-09-29 21:38:40 +0300

    Composer added

---
+++ ox.php
@@ -0,0 +1,22 @@
+#!/usr/bin/env php
+<?php
+/**
+ * @package     ox
+ * @copyright   Copyright (C) 2017 Zorcastudio. All rights reserved.
+ * @license     MIT License; see LICENSE file for details.
+ */
+
+namespace Ox;
+
+define('DS', '/');
+define('OX_ROOT', str_replace(DIRECTORY_SEPARATOR, DS, __DIR__));
+define('OX_VERSION', '0.0.0');
+define('OX_CONFIG_FOLDER', '/etc/ox');
+define('OX_DB_FOLDER', '/var/lib/ox');
+
+require OX_ROOT . '/vendor/autoload.php';
+
+$app = new App();
+$console = new ConsoleApp($app);
+
+$console->run();
