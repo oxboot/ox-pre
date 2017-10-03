6e55ae178001fb64bcc82c3ce6d9942090f4d24b
Zorca <vs@zorca.org>
2017-10-03 19:58:55 +0300

    PHPUnit tests added

---
+++ tests/Stack/StackTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     ox
+ * @copyright   Copyright (C) 2017 Zorcastudio. All rights reserved.
+ * @license     MIT License; see LICENSE file for details.
+ */
+
+namespace Ox\Stack;
+
+use PHPUnit\Framework\TestCase;
+
+class StackTest
+{
+
+}
